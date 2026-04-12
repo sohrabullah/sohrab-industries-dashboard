bd1f14669bfb73e08b877b21367f917d28b64961
Sohrab <sohrabullahkn687@gmail.com>
2026-04-11 22:11:43 -0700

    Create config.js

---
+++ sohrab-industries-dashboard/config.js
@@ -0,0 +1,4 @@
+// config.js
+const SUPABASE_URL = "https://czvxrjtdintvfjcgblxm.supabase.co";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...[Your Key]";
+const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
